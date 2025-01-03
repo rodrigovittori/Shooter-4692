c25c2535d992e795e2d618b6fc67ebad07155c68
Rodrigo Vittori <vittorirodrigo@gmail.com>
2025-01-03 18:01:57 -0300

    [M3.L2] · Actividad Nº 3 "Saltar y correr"
    Agregamos atributos y lógica necesarias para que nuestro PJ salte

--- PlayerController.cs
+++ PlayerController.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// [M3.L2] · Actividad Nº 2 "Controlando al personaje"
+// [M3.L2] · Actividad Nº 3 "Saltar y correr"
 
 public class PlayerController : MonoBehaviour
 {
 
-    [SerializeField] float movementSpeed = 7f;
-    private float currentSpeed;
+    [SerializeField] float movementSpeed = 7f; // Velocidad de movimiento base del jugador
+    [SerializeField] float shiftSpeed = 15f; // Velocidad de movimiento del jugador cuando corre
+    private float currentSpeed; // Velocidad de movimiento actual del jugador
+    [SerializeField] float jumpForce = 7f; // Altura del salto
+
+    private bool isGrounded = true; // flag que determina si estoy tocando una plataforma (o no)
     private Rigidbody rb;
     private Vector3 direction;
 
@@ -28,8 +32,20 @@ public class PlayerController : MonoBehaviour
 
         direction = new Vector3(moveHorizontal, 0.0f, moveVertical);
         direction = transform.TransformDirection(direction);
+
+        // Salto del PJ
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+            isGrounded = false;
+        }
     }
 
     private void FixedUpdate()
         { rb.MovePosition(transform.position + direction * currentSpeed * Time.deltaTime ); }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
+    }
 }
