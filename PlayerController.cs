b193e4473800973ce92a3282935375a74db8ec02
Rodrigo Vittori <vittorirodrigo@gmail.com>
2025-01-03 18:52:01 -0300

    [M3.L2] · Actividad Nº 7 (HomeWork) "Implementando resistencia"
    Agregamos mecánica de Sprint/Carrera

--- PlayerController.cs
+++ PlayerController.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// [M3.L2] · Actividad Nº 4 "Animando el movimiento!"
+// [M3.L2] · Actividad Nº 7 (HomeWork) "Implementando resistencia"
 
 public class PlayerController : MonoBehaviour
 {
-
     [SerializeField] float movementSpeed = 7f; // Velocidad de movimiento base del jugador
     [SerializeField] float shiftSpeed = 15f; // Velocidad de movimiento del jugador cuando corre
+    [SerializeField] float stamina = 5f; // cant. de segundos que puedo correr
+    [SerializeField] float staminaCooldown = 0.6f; // cant. de segundos que debo "respirar"/"descansar" tras agotar la Stamina
     private float currentSpeed; // Velocidad de movimiento actual del jugador
     [SerializeField] float jumpForce = 7f; // Altura del salto
 
@@ -29,13 +30,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Movimiento del PJ
+        // Leemos el input de Movimiento del PJ
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
+        // convertimos el Input en un vector para poder aplicarlo a nuestro RigidBody
         direction = new Vector3(moveHorizontal, 0.0f, moveVertical);
         direction = transform.TransformDirection(direction);
 
+        // Animación de movimiento:
         if (direction.x != 0 || direction.z != 0 )
             {anim.SetBool("Run", true);}
         
@@ -49,6 +52,29 @@ public class PlayerController : MonoBehaviour
             isGrounded = false;
             anim.SetBool("Jump", true);
         }
+
+        // Sprint
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            if (stamina >= staminaCooldown)
+            {
+                stamina -= Time.deltaTime; // Resto el tiempo que pasa corriendo
+                currentSpeed = shiftSpeed; // Velocidad = Correr
+            }
+            else
+                // Si yo presiono Shift, pero NO tengo stamina, (o la agoté recientemente) no corro
+                { currentSpeed = movementSpeed; } // Velocidad = caminar
+        }
+
+        else if (!Input.GetKey(KeyCode.LeftShift))
+            {   // Cuando NO pulso shift, recargo stamina
+                stamina += Time.deltaTime;
+                currentSpeed = movementSpeed;
+            } 
+
+        stamina = Mathf.Clamp(stamina, 0f, 5f);
+
+        Debug.Log("STAMINA: " + stamina + "| SPEED: " + currentSpeed);
     }
 
     private void FixedUpdate()
